88f12fea83cb95fd1e625e0aeb528c6410e63ade
eglucas <elysialucas@gmail.com>
2026-09-23 13:23:19 +0100

    Update page title

--- app.py
+++ app.py
@@ -25,7 +25,9 @@ current_dir = os.getcwd()
 # configure browser-tab-level settings for the entire app
 favicon = Image.open(f"{current_dir}/images/nesta_favicon.png")
 st.set_page_config(
-    page_title="Nesta Lifetime Cost Model", layout="wide", page_icon=favicon
+    page_title="Nesta Heating System Lifetime Cost Tool",
+    layout="wide",
+    page_icon=favicon,
 )
 
 # top banners
